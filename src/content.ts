244ff6f33329283385fa7b9ad751a37318fbc46f
lrobschram <lukeschram7@gmail.com>
2026-07-14 16:15:53 -0500

    Testing more scrapper methods

--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,5 @@
+import './App.css'
+
 console.log("Content Script loaded.");
 
 function scrapeSite() {
@@ -9,6 +11,35 @@ function scrapeSite() {
 
     return webInfo;
 }
+
+// function getHTMLheaders() {
+//   const h1s = document.querySelectorAll("h1");
+//   console.log("~~~~H1 HTML Elements~~~~");
+//   for (const header of h1s) {
+//     console.log(header);
+//   }
+
+//   const h2s = document.querySelectorAll("h2");
+//   console.log("~~~~H2 HTML Elements~~~~");
+//   for (const header of h2s) {
+//     console.log(header);
+//   }
+
+//   const h3s = document.querySelectorAll("h3");
+//   console.log("~~~~H3 HTML Elements~~~~");
+//   for (const header of h3s) {
+//     console.log(header);
+//   }
+// }
+
+function highlightLinks() {
+  const links = document.querySelectorAll("a");
+  for (const link of links) {
+    link.classList.add("highlight-link");
+  }
+}
+
+highlightLinks();
 
 // Listens for scan button click and sends basic info about the current website
 chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
